9fe6e05c69589bef2efdccb0da58335f8c243421
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-06-14 17:01:40 +0530

    Basic frontend Expense tracker

---
+++ Frontend/index.js
@@ -0,0 +1,18 @@
+const form = document.getElementsByTagName('form')[0];
+
+form.addEventListener('submit',formOnSubmit);
+
+
+function formOnSubmit(event){
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    axios.post('http://localhost:3000/signup',{
+        name:name,
+        email:email,
+        password:password
+    });
+
+}
