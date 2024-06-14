4775d02f95500fc92a7d3956116fb7fe087336ed
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-06-14 22:19:41 +0530

    Making frontend look somewhat better

--- Frontend/index/index.js
+++ Frontend/index/index.js
@@ -3,7 +3,7 @@ const form = document.getElementsByTagName('form')[0];
 
 
 form.addEventListener('submit',formOnSubmit);
-loginButton.addEventListener('submit',loginPressed);
+
 
 
 function formOnSubmit(event){
@@ -11,13 +11,17 @@ function formOnSubmit(event){
     const name = event.target.name.value;
     const email = event.target.email.value;
     const password = event.target.password.value;
+    const windowOut = window;
 
     axios.post('http://localhost:4000/user/signup',{
         name:name,
         email:email,
         password:password
     })
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data);
+        windowOut.location = '../login/login.html';
+    })
     .catch(err => {
         alert(err.response.data.error);
     })
