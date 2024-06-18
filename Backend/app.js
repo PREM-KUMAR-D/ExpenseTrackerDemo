e2be4c27d749843056acfd2991ef93dfe6f5dc96
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-06-18 12:36:51 +0530

    Removed some files by mistake fixing that issue

---
+++ Backend/app.js
@@ -0,0 +1,47 @@
+require('dotenv').config({ path: './Backend/.env', override: true });
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const database = require('./util/database');
+const userModel = require('./models/user');
+const expenseModel = require('./models/expense');
+const orderModel = require('./models/order');
+const userRoute = require('./routes/user');
+const expenseRoute = require('./routes/expense');
+const purchaseRoute = require('./routes/purchase');
+const premiumRoute = require('./routes/premium');
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use('/user', userRoute);
+
+app.use('/expense', expenseRoute);
+
+app.use('/purchase', purchaseRoute);
+
+app.use('/premium', premiumRoute);
+
+userModel.hasMany(expenseModel);
+expenseModel.belongsTo(userModel);
+
+userModel.hasMany(orderModel);
+orderModel.belongsTo(userModel);
+
+
+database
+    .sync()
+    // .sync({ force: true })
+    .then(() => {
+
+        app.listen(process.env.PORT);
+    })
+    .catch(err => console.log(err));
+
+
